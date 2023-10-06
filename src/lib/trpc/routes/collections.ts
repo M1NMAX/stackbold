25cb836d893c8272f9aa7bf3b9ae8dde76676c47
M1NMAX <josecferreira.alt@gmail.com>
2023-10-06 09:08:40 +0100

    ref trpc folder struct

--- src/lib/trpc/routes/collections.ts
+++ src/lib/trpc/routes/collections.ts
@@ -1,16 +1,11 @@
-import { t } from '$lib/trpc/t';
-import { logger } from '$lib/trpc/middleware/logger';
-import { auth } from '$lib/trpc/middleware/auth';
+import { createTRPCRouter, protectedProcedure } from '$lib/trpc/t';
 import { prisma } from '$lib/server/prisma';
 
-export const collections = t.router({
-	getUserCollections: t.procedure
-		.use(logger)
-		.use(auth)
-		.query(({ ctx: { userId } }) =>
-			prisma.collection.findMany({
-				where: { ownerId: userId },
-				orderBy: { name: 'asc' }
-			})
-		)
+export const collections = createTRPCRouter({
+	getUserCollections: protectedProcedure.query(({ ctx: { userId } }) =>
+		prisma.collection.findMany({
+			where: { ownerId: userId },
+			orderBy: { name: 'asc' }
+		})
+	)
 });
--- src/lib/trpc/t.ts
+++ src/lib/trpc/t.ts
@@ -1,4 +1,24 @@
 import type { Context } from '$lib/trpc/context';
-import { initTRPC } from '@trpc/server';
+import { TRPCError, initTRPC } from '@trpc/server';
 
 export const t = initTRPC.context<Context>().create();
+
+export const loggerMiddleware = t.middleware(async ({ path, type, next }) => {
+	const start = Date.now();
+	const result = await next();
+	const ms = Date.now() - start;
+	console.log(`${result.ok ? 'OK' : 'ERR'} ${type} ${path} - ${ms}ms`);
+
+	return result;
+});
+
+export const authMiddleware = t.middleware(async ({ next, ctx }) => {
+	if (!ctx.userId) throw new TRPCError({ code: 'UNAUTHORIZED' });
+	return next();
+});
+
+export const createTRPCRouter = t.router;
+
+export const publicProcedure = t.procedure.use(loggerMiddleware);
+
+export const protectedProcedure = t.procedure.use(loggerMiddleware).use(authMiddleware);
