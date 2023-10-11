f9566b44e7c7c949240be0bcd92dccc1c6f03903
M1NMAX <josecferreira.alt@gmail.com>
2023-10-11 13:23:17 +0100

    added items trpc route

--- src/lib/trpc/router.ts
+++ src/lib/trpc/router.ts
@@ -1,9 +1,11 @@
 import { collections } from '$lib/trpc/routes/collections';
+import { items } from '$lib/trpc/routes/items';
 import { t } from '$lib/trpc/t';
 import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
 
 export const router = t.router({
-	collections
+	collections,
+	items
 });
 
 export type Router = typeof router;
---
+++ src/lib/trpc/routes/items.ts
@@ -0,0 +1,9 @@
+import { prisma } from '$lib/server/prisma';
+import { createTRPCRouter, protectedProcedure } from '$lib/trpc/t';
+import { z } from 'zod';
+
+export const items = createTRPCRouter({
+	getCollectionItems: protectedProcedure
+		.input(z.string())
+		.query(({ input }) => prisma.item.findMany({ where: { collectionId: input } }))
+});
