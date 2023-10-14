243ae2bbb2acff4840addd678a8465ed6999a2d4
M1NMAX <josecferreira.alt@gmail.com>
2023-10-14 17:17:45 +0100

    added iconbtn component

---
+++ src/lib/components/IconBtn.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Button } from 'flowbite-svelte';
+
+	export let extraClass = '';
+</script>
+
+<Button on:click color="alternative" class={`${extraClass} p-1.5 rounded `}>
+	<slot />
+</Button>
