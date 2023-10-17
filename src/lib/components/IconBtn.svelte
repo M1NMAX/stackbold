69bb946d6aa916658fee8e7375ad0a6331d60c2d
M1NMAX <josecferreira.alt@gmail.com>
2023-10-17 14:31:55 +0100

    ref icon Btn

--- src/lib/components/IconBtn.svelte
+++ src/lib/components/IconBtn.svelte
@@ -1,9 +1,31 @@
 <script lang="ts">
-	import { Button } from 'flowbite-svelte';
+	import type { Colors, Size, Sizes } from '$lib/types';
+	import { Button, Tooltip } from 'flowbite-svelte';
 
+	export let id: string = '';
+	export let tootipText: string | null = null;
+	export let color: string = 'gray';
+	export let size: Size = 'xs';
 	export let extraClass = '';
+
+	const sizes: Sizes = {
+		xxs: 'w-4 h-4',
+		xs: 'w-6 h-6'
+	};
+	const colors: Colors = {
+		gray: 'bg-gray-200 hover:bg-gray-300 ',
+		alternative: 'bg-gray-100 hover:bg-gray-50'
+	};
 </script>
 
-<Button on:click color="alternative" class={`${extraClass} p-1.5 rounded `}>
+<Button
+	{id}
+	on:click
+	class={`${colors[color]} ${sizes[size]} text-black  p-4 rounded focus:ring-0 border-0 ${extraClass}`}
+>
 	<slot />
 </Button>
+
+{#if tootipText}
+	<Tooltip>{tootipText}</Tooltip>
+{/if}
--- src/lib/components/property/Label.svelte
+++ src/lib/components/property/Label.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import { Button, Label } from 'flowbite-svelte';
 	import { DotsVerticalOutline } from 'flowbite-svelte-icons';
+	import IconBtn from '../IconBtn.svelte';
 
 	export let id: string;
 	export let name: string;
 </script>
 
 <div class="grow flex justify-between items-center">
-	<Label for={id} class="grow truncate font-semibold">{name}</Label>
-	<Button color="alternative" class="p-1 rounded">
-		<DotsVerticalOutline class="icon-xxs" />
-	</Button>
+	<Label for={id} class="grow truncate font-semibold text-base">{name}</Label>
+	<IconBtn color="alternative" size="xxs">
+		<DotsVerticalOutline />
+	</IconBtn>
 </div>
