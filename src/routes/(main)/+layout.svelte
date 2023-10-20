8eb667f2d28da81d64fc8ffb116b9a8130b0ffd9
M1NMAX <josecferreira.alt@gmail.com>
2023-10-20 15:21:46 +0100

    upd sidebar ui

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -11,8 +11,13 @@
 		Input
 	} from 'flowbite-svelte';
 	import {
+		CaretSortSolid,
+		ChervonDoubleRightSolid,
+		ChevronSortOutline,
+		ChevronSortSolid,
 		CogOutline,
 		DnaOutline,
+		GridOutline,
 		HomeOutline,
 		PlusOutline,
 		SearchOutline
@@ -25,6 +30,7 @@
 	import { TRPCClientError } from '@trpc/client';
 	import { goto, invalidateAll } from '$app/navigation';
 	import type { LayoutData } from './$types';
+	import IconBtn from '$lib/components/IconBtn.svelte';
 
 	export let data: LayoutData;
 
@@ -63,18 +69,19 @@
 
 <div class=" h-screen flex bg-gray-200">
 	<Sidebar class="w-1/6">
-		<SidebarWrapper class="h-full pr-0 rounded-none bg-gray-200">
+		<SidebarWrapper class="h-full flex flex-col space-y-4 px-2 py-1.5  rounded-none bg-gray-200">
 			<SidebarGroup>
-				<div class="flex justify-between space-x-4">
-					<Avatar src="./favicon.png" class="h-8 w-8" />
-					<span class="text-xl font-semibold"> {data.user.name} </span>
-					<Badge color="blue" class="font-bold uppercase">Admin</Badge>
-				</div>
-
-				<div class="py-1 pr-2 pl-1">
-					<button class="w-full flex items-center space-x-4 p-1.5 rounded-md bg-gray-300">
+				<div class="flex space-x-1">
+					<button class="relative px-1 py-0.5 rounded bg-gray-300 hover:bg-gray-100 text-black">
+						<Avatar
+							src={`https://api.dicebear.com/7.x/shapes/svg?seed=${data.user.name}`}
+							class=" rounded-full h-7 w-7"
+						/>
+						<CaretSortSolid class="absolute right-0 bottom-0 w-3 h-3" />
+					</button>
+					<button class="grow flex items-center space-x-4 p-1.5 rounded-md bg-gray-300">
 						<SearchOutline class="icon-xss" />
-						<span class="font-semibold"> Search </span>
+						<span class="font-semibold"> Quick Search </span>
 					</button>
 				</div>
 
@@ -90,47 +97,56 @@
 					</svelte:fragment>
 				</SidebarItem>
 
-				<SidebarItem label="Settings" href="/settings" active={activeUrl === '/settings'}>
+				<SidebarItem
+					label="My Collections"
+					href="/collections"
+					active={activeUrl === '/collections'}
+				>
 					<svelte:fragment slot="icon">
-						<CogOutline class="icon-sm" />
+						<GridOutline class="icon-sm" />
 					</svelte:fragment>
 				</SidebarItem>
 			</SidebarGroup>
 
-			<SidebarGroup class="mt-8">
+			<!-- <SidebarGroup>
 				<span class="flex justify-between">
 					<span> Favourites </span>
 
 					<span>
 						{favourites.length}
 					</span>
+					<IconBtn><PlusOutline /></IconBtn>
 				</span>
 				{#each favourites as collection}
 					<SidebarCollection {collection} active={activeCollection(collection.id)} />
 				{/each}
-			</SidebarGroup>
+			</SidebarGroup> -->
 
-			<SidebarGroup class="mt-8">
-				<span class="flex justify-between">
-					<span> Collections </span>
-					<span>
-						{data.collections.length}
-					</span>
+			<SidebarGroup class="grow overflow-y-auto">
+				<span class="sticky top-0 flex justify-between items-center bg-gray-200">
+					<a
+						href="/collections"
+						class={`${
+							activeUrl === '/collections' && 'bg-gray-300 dark:bg-gray-600'
+						} font-semibold text-sm grow p-1 rounded hover:bg-gray-300
+					dark:hover:bg-gray-500 transition duration-75 dark:text-gray-400`}
+					>
+						All Collections
+					</a>
+					<IconBtn
+						on:click={() => (createCollectionModal = true)}
+						tootipText="Create collection"
+						size="xxs"
+					>
+						<PlusOutline />
+					</IconBtn>
 				</span>
-				{#each data.collections as collection}
-					<SidebarCollection {collection} active={activeCollection(collection.id)} />
-				{/each}
+				<div>
+					{#each data.collections as collection}
+						<SidebarCollection {collection} active={activeCollection(collection.id)} />
+					{/each}
+				</div>
 			</SidebarGroup>
-
-			<div class="py-1 pr-2 pl-1">
-				<Button
-					on:click={() => (createCollectionModal = true)}
-					class="w-full flex items-center space-x-4 p-1.5 rounded-md"
-				>
-					<PlusOutline class="icon-xss" />
-					<span class="font-semibold"> Create Collection </span>
-				</Button>
-			</div>
 		</SidebarWrapper>
 	</Sidebar>
 
