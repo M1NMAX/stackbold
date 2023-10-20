7e96c017d1c8061462d392f03bd398c06d5ba0c1
M1NMAX <josecferreira.alt@gmail.com>
2023-10-20 15:13:59 +0100

    upd collection page ui

--- src/routes/(main)/collections/+page.svelte
+++ src/routes/(main)/collections/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-	import { Button, Card, Search } from 'flowbite-svelte';
+	import { Button } from 'flowbite-svelte';
 	import type { PageData } from './$types';
+	import { goto } from '$app/navigation';
+	import { FolderOutline } from 'flowbite-svelte-icons';
 
 	export let data: PageData;
 </script>
@@ -9,18 +11,29 @@
 	<title>Collections * Stackbold</title>
 </svelte:head>
 
-<div class="flex flex-col space-y-4">
-	<Search>
-		<Button>Search</Button>
-	</Search>
-	{#each data.collections as collection}
-		<Card href={`/collections/${collection.id}`}>
-			<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-				{collection.name}
-			</h5>
-			<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
-				{collection.description}
-			</p>
-		</Card>
-	{/each}
+<div class="grow m-2 p-2 rounded-md bg-gray-50">
+	<div class="h-full flex flex-col justify-between p-1 overflow-hidden">
+		<h1 class=" font-semibold text-2xl">My Collections</h1>
+
+		<div class="grow flex flex-col space-y-2 overflow-y-auto">
+			{#each data.collections as collection}
+				<div class="flex flex-col rounded bg-gray-100 py-1 px-2">
+					<div class="flex items-center justify-between space-x-2">
+						<FolderOutline class="w-4 h-4" />
+						<h5 class="grow mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+							{collection.name}
+						</h5>
+						<Button
+							on:click={() => goto(`/collections/${collection.id}`)}
+							class="bg-gray-50 hover:bg-gray-100 font-semibold text-black px-2 py-1">Open</Button
+						>
+					</div>
+
+					<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+						{collection.description}
+					</p>
+				</div>
+			{/each}
+		</div>
+	</div>
 </div>
