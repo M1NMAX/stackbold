c8c5326ce7fd8e072811147465b68ac7530743b8
M1NMAX <josecferreira.alt@gmail.com>
2023-10-24 09:23:15 +0100

    add collection property procedures

--- src/lib/trpc/routes/collections.ts
+++ src/lib/trpc/routes/collections.ts
@@ -2,7 +2,13 @@ import { createTRPCRouter, protectedProcedure } from '$lib/trpc/t';
 import { prisma } from '$lib/server/prisma';
 import { z } from 'zod';
 import { TRPCError } from '@trpc/server';
-import { CollectionCreateWithoutOwnerInputSchema, CollectionUpdateInputSchema } from '$prisma-zod';
+import {
+	CollectionCreateWithoutOwnerInputSchema,
+	CollectionPropertyCreateInputSchema,
+	CollectionUpdateInputSchema,
+	ColorSchema,
+	PropertyTypeSchema
+} from '$prisma-zod';
 
 export const collections = createTRPCRouter({
 	getUserCollections: protectedProcedure.query(({ ctx: { userId } }) =>
@@ -24,7 +30,6 @@ export const collections = createTRPCRouter({
 	updateCollection: protectedProcedure
 		.input(z.object({ id: z.string(), data: CollectionUpdateInputSchema }))
 		.mutation(async ({ input: { id, data }, ctx: userId }) => {
-			console.table(data);
 			await prisma.collection.update({ data, where: { id } });
 		}),
 
@@ -36,5 +41,49 @@ export const collections = createTRPCRouter({
 			if (collection.ownerId !== userId) throw new TRPCError({ code: 'UNAUTHORIZED' });
 
 			await prisma.collection.delete({ where: { id } });
+		}),
+
+	addProperty: protectedProcedure
+		.input(z.object({ id: z.string(), property: CollectionPropertyCreateInputSchema }))
+		.mutation(async ({ input: { id, property }, ctx: { userId } }) => {
+			prisma.collection.update({ data: { properties: { push: [property] } }, where: { id } });
+		}),
+
+	updateProperty: protectedProcedure
+		.input(
+			z.object({
+				id: z.string(),
+				property: z.object({
+					id: z.string(),
+					name: z.string().optional(),
+					type: PropertyTypeSchema.optional(),
+					options: z
+						.array(
+							z.object({
+								id: z.string().optional(),
+								value: z.string(),
+								color: z.lazy(() => ColorSchema).optional()
+							})
+						)
+						.optional()
+				})
+			})
+		)
+		.mutation(async ({ input: { id, property }, ctx: { userId } }) => {
+			const { id: pid, ...rest } = property;
+
+			await prisma.collection.update({
+				where: { id },
+				data: { properties: { updateMany: { where: { id: pid }, data: rest } } }
+			});
+		}),
+
+	deleteProperty: protectedProcedure
+		.input(z.object({ id: z.string(), propertyId: z.string() }))
+		.mutation(async ({ input: { id, propertyId }, ctx: { userId } }) => {
+			await prisma.collection.update({
+				data: { properties: { deleteMany: { where: { id: propertyId } } } },
+				where: { id }
+			});
 		})
 });
