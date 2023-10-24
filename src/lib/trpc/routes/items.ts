d1637f5a7db7200c27fb7bf8930b150d3cff61bd
M1NMAX <josecferreira.alt@gmail.com>
2023-10-24 09:23:34 +0100

    add item properties procedures

--- src/lib/trpc/routes/items.ts
+++ src/lib/trpc/routes/items.ts
@@ -1,8 +1,8 @@
 import { prisma } from '$lib/server/prisma';
 import { createTRPCRouter, protectedProcedure } from '$lib/trpc/t';
 import {
-	ItemCreateInputSchema,
 	ItemCreateWithoutCollectionInputSchema,
+	ItemPropertyCreateInputSchema,
 	ItemUpdateInputSchema
 } from '$prisma-zod';
 import { TRPCError } from '@trpc/server';
@@ -46,5 +46,38 @@ export const items = createTRPCRouter({
 			if (item.collection.ownerId !== userId) throw new TRPCError({ code: 'UNAUTHORIZED' });
 
 			await prisma.item.delete({ where: { id } });
+		}),
+	addProperty: protectedProcedure
+		.input(z.object({ id: z.string(), property: ItemPropertyCreateInputSchema }))
+		.mutation(async ({ input: { id, property }, ctx: { userId } }) => {
+			await prisma.item.update({
+				data: { properties: { push: [property] } },
+				where: { id }
+			});
+		}),
+	updateProperty: protectedProcedure
+		.input(
+			z.object({
+				id: z.string(),
+				property: z.object({ id: z.string(), value: z.string() })
+			})
+		)
+		.mutation(async ({ input: { id, property }, ctx: { userId } }) => {
+			const { id: pid, ...rest } = property;
+
+			await prisma.item.update({
+				where: { id },
+				data: {
+					properties: { updateMany: { where: { id: pid }, data: rest } }
+				}
+			});
+		}),
+	deleteProperty: protectedProcedure
+		.input(z.object({ id: z.string(), propertyId: z.string() }))
+		.mutation(async ({ input: { id, propertyId }, ctx: { userId } }) => {
+			await prisma.item.update({
+				data: { properties: { deleteMany: { where: { id: propertyId } } } },
+				where: { id }
+			});
 		})
 });
