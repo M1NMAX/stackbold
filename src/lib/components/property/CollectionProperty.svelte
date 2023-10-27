92ef8da4b5e2a803716cccd54cb078fec5356241
M1NMAX <josecferreira.alt@gmail.com>
2023-10-27 10:51:49 +0100

    ref collection prop ui and add event forward

--- src/lib/components/property/CollectionProperty.svelte
+++ src/lib/components/property/CollectionProperty.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
 	import { PropertyType, type CollectionProperty } from '@prisma/client';
 	import Label from './Label.svelte';
-	import type { InputType } from 'flowbite-svelte';
 
 	export let property: CollectionProperty;
 	export let value: string;
 
-	const propType = property.type.toLowerCase() as InputType;
-
-	const inputClass = 'rounded-sm bg-gray-200 border-none text-base font-semibold py-0 px-1';
+	const propType = property.type.toLowerCase();
 </script>
 
 {#if property.type === PropertyType.CHECKBOX}
@@ -17,16 +14,16 @@
 			id={property.id}
 			type="checkbox"
 			checked={value === 'true'}
-			class="checkbox rounded border checkbox-primary text-3xl focus:ring-0"
+			class="checkbox checkbox-sm checkbox-primary"
 		/>
-		<Label id={property.id} name={property.name} />
+		<Label id={property.id} name={property.name} on:edit on:duplicate on:delete />
 	</div>
 {:else}
 	<div class="form-control rounded bg-gray-200 px-2 pb-2 pt-0">
-		{#if property.type === PropertyType.SELECT}
-			<Label id={property.id} name={property.name} />
+		<Label id={property.id} name={property.name} on:edit on:duplicate on:delete />
 
-			<select {value} class={` select select-ghost ${inputClass}`}>
+		{#if property.type === PropertyType.SELECT}
+			<select {value} class="select select-sm select-ghost">
 				{#each property.options as option}
 					<option value={option.value}>
 						{option.value}
@@ -34,18 +31,20 @@
 				{/each}
 			</select>
 		{:else if property.type === PropertyType.TEXT}
-			<Label id={property.id} name={property.name} />
-
 			<textarea
 				id={property.id}
 				name={property.name}
 				{value}
 				rows="3"
-				class={` textarea textarea-ghost ${inputClass}`}
+				class="textarea textarea-sm textarea-ghost font-medium"
 			/>
 		{:else}
-			<Label id={property.id} name={property.name} />
-			<input id={property.id} type={propType} {value} class={`input input-ghost ${inputClass}`} />
+			<input
+				id={property.id}
+				type={propType}
+				{value}
+				class="input input-sm input-ghost font-medium"
+			/>
 		{/if}
 	</div>
 {/if}
