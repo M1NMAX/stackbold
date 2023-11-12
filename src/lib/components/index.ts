bf6e8925e0270c44b398b90969545fb4f27c7000
M1NMAX <josecferreira.alt@gmail.com>
2023-11-12 09:18:13 +0000

    ref items table view

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -8,6 +8,9 @@ export { default as DropdownItem } from './Dropdown/DropdownItem.svelte';
 export { default as IconBtn } from './IconBtn.svelte';
 export { default as ItemProperty } from './property/ItemProperty.svelte';
 
+export { default as ItemsListView } from './items/ItemsListView.svelte';
+export { default as ItemsTableView } from './items/ItemsTableView.svelte';
+
 export { default as RadioButton } from './input/RadioButton.svelte';
 
 export { default as Modal } from './Modal.svelte';
@@ -15,5 +18,7 @@ export { default as Modal } from './Modal.svelte';
 export { default as Sidebar } from './sidebar/Sidebar.svelte';
 export { default as SidebarCollection } from './sidebar/SidebarCollection.svelte';
 export { default as SidebarItem } from './sidebar/SidebarItem.svelte';
+
+export { default as SortArrow } from './SortArrow.svelte';
 
 export { default as Textarea } from './Textarea/Textarea.svelte';
---
+++ src/lib/components/items/ItemsTableView.svelte
@@ -0,0 +1,121 @@
+<script lang="ts">
+	import type { IBaseSchema, OrderType } from '$lib/utils';
+	import type { CollectionProperty, Item, ItemProperty as ItemPropertyType } from '@prisma/client';
+	import SortArrow from '../SortArrow.svelte';
+	import dayjs from '$lib/dayjs';
+	import IconBtn from '../IconBtn.svelte';
+	import { DotsHorizontalOutline } from 'flowbite-svelte-icons';
+	import { fade } from 'svelte/transition';
+	import ItemProperty from '../property/ItemProperty.svelte';
+
+	export let active: boolean = false;
+
+	export let items: Item[];
+	export let collectionProperties: CollectionProperty[];
+	export let order: OrderType = 'asc';
+
+	export let onClickTableHead: (field: keyof IBaseSchema) => void;
+
+	const getItemProperty = (pid: string, properties: ItemPropertyType[]) => {
+		return properties.find((property) => property.id === pid) || null;
+	};
+
+	const getOptionValue = (property: CollectionProperty, value: string) => {
+		const option = property.options.find((opt) => opt.id === value);
+		return option ? option.value : '';
+	};
+
+	const getOptionColor = (property: CollectionProperty, value: string) => {
+		const option = property.options.find((opt) => opt.id === value);
+		return option ? option.color : 'GRAY';
+	};
+</script>
+
+<table class="w-full">
+	<thead>
+		<tr class="text-gray-500 text-sm">
+			<th scope="col" class="text-left rounded-t-md hover:bg-base-200 py-2 px-1 cursor-pointer">
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<div on:click={() => onClickTableHead('name')} class="flex justify-between items-center">
+					<span> Name </span>
+
+					<SortArrow bind:order />
+				</div>
+			</th>
+			{#each collectionProperties as property (property.id)}
+				<th scope="col" class="text-left rounded-t-md hover:bg-base-200 py-2 px-1 cursor-pointer">
+					{property.name}
+				</th>
+			{/each}
+			<th
+				scope="col"
+				class="whitespace-nowrap text-left rounded-t-md hover:bg-base-200 py-2 px-1 cursor-pointer"
+			>
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<div
+					on:click={() => onClickTableHead('updatedAt')}
+					class="flex justify-between items-center"
+				>
+					<span> Last updated </span>
+					<SortArrow bind:order />
+				</div>
+			</th>
+
+			<th scope="col" class="text-left" title="Row actions">
+				<DotsHorizontalOutline />
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#if items.length}
+			{#each items as item (item.id)}
+				<tr class="font-medium text-base border-y-2 border-gray-100 hover:bg-base-200">
+					<td class="text-left py-2 px-1"> {item.name}</td>
+
+					{#each collectionProperties as property (property.id)}
+						{@const itemProperty = getItemProperty(property.id, item.properties)}
+						<td class="text-left py-2 px-1">
+							{#if itemProperty}
+								<ItemProperty
+									itemId={item.id}
+									{property}
+									color={property.type === 'SELECT'
+										? getOptionColor(property, itemProperty.value)
+										: 'GRAY'}
+									value={property.type === 'SELECT'
+										? getOptionValue(property, itemProperty.value)
+										: itemProperty.value}
+									on:updPropertyValue
+								/>
+							{/if}
+						</td>
+					{/each}
+					<td class="whitespace-nowrap text-left py-2 px-1">
+						{dayjs(item.updatedAt).fromNow()}
+					</td>
+
+					<td class="text-left whitespace-nowrap">
+						<!-- <IconBtn>
+                            <WindowOutline class="rotate-90" />
+                        </IconBtn> -->
+
+						<IconBtn>
+							<DotsHorizontalOutline />
+						</IconBtn>
+					</td>
+				</tr>
+			{/each}
+		{:else}
+			<tr>
+				<td colspan={collectionProperties.length + 3}>
+					<div class="empty" in:fade>
+						<!-- <div class="empty-icon"><IconEmpty size="5em" /></div> -->
+						No items found.
+					</div>
+				</td>
+			</tr>
+		{/if}
+	</tbody>
+</table>
