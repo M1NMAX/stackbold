0d0d9a81b0913555b49ff40029d97b0fadd693f7
M1NMAX <josecferreira.alt@gmail.com>
2023-11-13 10:56:48 +0000

    ref sort funs

--- src/lib/utils/sort.ts
+++ src/lib/utils/sort.ts
@@ -7,16 +7,11 @@ export interface IBaseSchema {
 
 export type OrderType = 'asc' | 'desc';
 
-const sortDate = <
-	T extends IBaseSchema,
-	K extends keyof Pick<IBaseSchema, 'createdAt' | 'updatedAt'>
->(
-	a: T,
-	b: T,
-	field: K
-) => {
-	const left = a[field];
-	const right = b[field];
+type DateKey<T> = keyof T & (T[keyof T] extends Date ? keyof T : never);
+
+const sortDate = <T extends IBaseSchema, K extends DateKey<T>>(a: T, b: T, field: K) => {
+	const left = a[field] as Date;
+	const right = b[field] as Date;
 
 	if (left.getTime() < right.getTime()) return -1;
 	if (left.getTime() > right.getTime()) return 1;
@@ -42,7 +37,7 @@ const sortFun = <K extends keyof IBaseSchema>(field: K, order: OrderType) => {
 		let result = 0;
 
 		if (typeof a[field] === 'string') result = sortString(a, b, field);
-		else if (a[field] instanceof Date) result = sortDate(a, b, 'createdAt');
+		else if (a[field] instanceof Date) result = sortDate(a, b, field as DateKey<IBaseSchema>);
 
 		return order === 'asc' ? +result : -result;
 	};
