9157794d1196b6f949d8c3f03b46e6dfe9ea304a
M1NMAX <josecferreira.alt@gmail.com>
2023-11-19 10:53:11 +0000

    ref property option to use combobox instead custom colorPicker

--- src/lib/components/property/Option.svelte
+++ src/lib/components/property/Option.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	import { ColorPicker } from '$lib/components';
+	import { Check, Trash } from 'lucide-svelte';
 	import { createEventDispatcher } from 'svelte';
+	import { PROPERTY_COLORS } from '$lib/constant';
 	import type { Color, Option } from '@prisma/client';
-	import Button from '$lib/components/ui/button/button.svelte';
-	import { Trash } from 'lucide-svelte';
+	import { tick } from 'svelte';
+	import { capitalizeFirstLetter, cn } from '$lib/utils';
+	import * as Command from '$lib/components/ui/command';
+	import * as Popover from '$lib/components/ui/popover';
+	import { Button } from '$lib/components/ui/button';
 
 	export let propertyId: string;
 	export let option: Option;
@@ -18,13 +22,59 @@
 		const targetEl = e.target as HTMLInputElement;
 		dispatch('updOptValue', { propertyId, optionId: option.id, value: targetEl.value });
 	};
+
+	let open = false;
+
+	let value = option.color as string;
+	$: selectedKey = (Object.keys(PROPERTY_COLORS).find((key) => key === value) as Color) ?? 'GRAY';
+
+	const handleSelectColor = (currValue: string, triggerId: string) => {
+		value = currValue;
+		dispatch('updOptColor', { propertyId, optionId: option.id, color: value as Color });
+
+		// Refocus the trigger btn when user selects and item from the list,
+		// so users can navigating using the keyboard
+		open = false;
+		tick().then(() => {
+			document.getElementById(triggerId)?.focus();
+		});
+	};
 </script>
 
 <div class="w-full flex justify-between items-center space-x-1.5">
-	<ColorPicker
-		value={option.color}
-		onChange={(color) => dispatch('updOptColor', { propertyId, optionId: option.id, color })}
-	/>
+	<Popover.Root bind:open let:ids>
+		<Popover.Trigger asChild let:builder>
+			<Button
+				builders={[builder]}
+				variant="outline"
+				size="xs"
+				role="combobox"
+				aria-expanded={open}
+				class={`${PROPERTY_COLORS[selectedKey]} h-6 w-6 rounded`}
+			/>
+		</Popover.Trigger>
+		<Popover.Content class="w-[200px] p-0">
+			<Command.Root>
+				<Command.Input placeholder="Search colors..." />
+				<Command.Empty>No color found.</Command.Empty>
+				<Command.Group>
+					{#each Object.entries(PROPERTY_COLORS) as [key, value]}
+						<Command.Item
+							value={key}
+							onSelect={(currentValue) => handleSelectColor(currentValue, ids.trigger)}
+							class="space-x-2"
+						>
+							<Check class={cn('mr-2 icon-xxs', key !== selectedKey && 'text-transparent')} />
+							<span class={`${value} h-5 w-5`} />
+							<span>
+								{capitalizeFirstLetter(key)}
+							</span>
+						</Command.Item>
+					{/each}
+				</Command.Group>
+			</Command.Root>
+		</Popover.Content>
+	</Popover.Root>
 
 	<input
 		name="option"
@@ -33,7 +83,12 @@
 		class="grow input input-xs text-sm font-semibold input-ghost"
 	/>
 
-	<Button on:click={() => dispatch('deleteOpt', { propertyId, optionId: option.id })}>
-		<Trash class="w-3 h-3" />
+	<Button
+		variant="destructive"
+		size="xs"
+		on:click={() => dispatch('deleteOpt', { propertyId, optionId: option.id })}
+		class="rounded-md"
+	>
+		<Trash class="icon-xss" />
 	</Button>
 </div>
