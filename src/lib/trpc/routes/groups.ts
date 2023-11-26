1ca7ef372f3c50c5ac8eefde1a39bbc5ac7c9056
M1NMAX <josecferreira.alt@gmail.com>
2023-11-26 11:34:20 +0000

    add create, upd and delete procedure to groups route

--- src/lib/trpc/routes/groups.ts
+++ src/lib/trpc/routes/groups.ts
@@ -1,5 +1,8 @@
 import { createTRPCRouter, protectedProcedure } from '$lib/trpc/t';
 import { prisma } from '$lib/server/prisma';
+import { GroupCreateWithoutOwnerInputSchema, GroupUpdateInputSchema } from '$prisma-zod';
+import { z } from 'zod';
+import { TRPCError } from '@trpc/server';
 
 export const groups = createTRPCRouter({
 	list: protectedProcedure.query(({ ctx: { userId } }) => {
@@ -7,5 +10,25 @@ export const groups = createTRPCRouter({
 			where: { ownerId: userId },
 			orderBy: { name: 'asc' }
 		});
+	}),
+
+	create: protectedProcedure.input(GroupCreateWithoutOwnerInputSchema).mutation(
+		async ({ input: groupData, ctx: { userId } }) =>
+			await prisma.group.create({
+				data: { ownerId: userId, ...groupData }
+			})
+	),
+	update: protectedProcedure
+		.input(z.object({ id: z.string(), data: GroupUpdateInputSchema }))
+		.mutation(async ({ input: { id, data }, ctx: userId }) => {
+			await prisma.group.update({ data, where: { id } });
+		}),
+
+	delete: protectedProcedure.input(z.string()).mutation(async ({ input: id, ctx: { userId } }) => {
+		const group = await prisma.group.findUniqueOrThrow({ where: { id } });
+
+		if (group.ownerId !== userId) throw new TRPCError({ code: 'UNAUTHORIZED' });
+
+		await prisma.group.delete({ where: { id } });
 	})
 });
