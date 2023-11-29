5ae1810570e3cdd5263e35951fcfa0f88c4bd8a5
M1NMAX <josecferreira.alt@gmail.com>
2023-11-29 10:25:16 +0000

    remove unused dependencies

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import { Toaster } from 'svelte-french-toast';
-	import { onMount } from 'svelte';
-	import { themeChange } from 'theme-change';
 	import { ModeWatcher } from 'mode-watcher';
-
-	onMount(() => {
-		themeChange(false); //  false parameter is required for svelte
-	});
 </script>
 
 <ModeWatcher />
