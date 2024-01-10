e4785c792b1e640e2533e1fcab8a96273162b381
M1NMAX <josecferreira.alt@gmail.com>
2024-01-10 09:29:17 +0000

    code clean up

--- src/routes/(main)/+page.server.ts
+++ src/routes/(main)/+page.server.ts
@@ -1,9 +1,6 @@
 import { fail, redirect } from '@sveltejs/kit';
 import { auth } from '$lib/server/lucia';
-import { router } from '$lib/trpc/router';
-import { createContext } from '$lib/trpc/context';
-import type { Actions, PageServerLoad } from './$types';
-import type { Item } from '@prisma/client';
+import type { Actions } from './$types';
 
 export const actions: Actions = {
 	logout: async ({ locals }) => {
@@ -13,22 +10,4 @@ export const actions: Actions = {
 		locals.auth.setSession(null); // remove cookie
 		redirect(302, '/signin'); // redirect to login page
 	}
-};
-
-export const load: PageServerLoad = async (event) => {
-	const { collections } = await event.parent();
-
-	let items: Record<string, number> = {};
-
-	for (const collection of collections) {
-		const tempItems = await router
-			.createCaller(await createContext(event))
-			.items.list(collection.id);
-
-		if (!items[collection.id]) items[collection.id] = 0;
-
-		items[collection.id] = tempItems.length;
-	}
-
-	return { collections, items };
 };
