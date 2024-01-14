0475db03871b15727d46f3cc05a1174a08cbb06c
M1NMAX <josecferreira.alt@gmail.com>
2024-01-14 18:14:32 +0000

    pwa setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,30 @@
 <script lang="ts">
 	import '../app.css';
 
+	import { onMount } from 'svelte';
 	import Toaster from '$lib/components/feedback/Toaster.svelte';
 	import { ModeWatcher } from 'mode-watcher';
+
+	async function detectSWUpdate() {
+		const registration = await navigator.serviceWorker.ready;
+
+		registration.addEventListener('updatefound', () => {
+			const newSW = registration.installing;
+
+			newSW?.addEventListener('statechange', () => {
+				if (newSW.state === 'installed') {
+					if (confirm('New update available! Reload to update?')) {
+						newSW.postMessage({ type: 'SKIP_WAITING' });
+						window.location.reload();
+					}
+				}
+			});
+		});
+	}
+
+	onMount(() => {
+		detectSWUpdate();
+	});
 </script>
 
 <ModeWatcher />
