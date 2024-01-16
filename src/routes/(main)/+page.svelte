c39db21b71806b41f5b43d43ad4f8f83239ef55f
M1NMAX <josecferreira.alt@gmail.com>
2024-01-16 18:59:53 +0000

    upd home page

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,17 +1,25 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { PageContainer, PageContent, PageHeader } from '$lib/components/page';
-	import { ArrowRight } from 'lucide-svelte';
+	import { ArrowRight, Dna, Egg, FolderPlus, Frown, HeartCrack } from 'lucide-svelte';
 	import { Button } from '$lib/components/ui/button';
 	import { CollectionOverview } from '$lib/components/collection';
 	import type { RouterOutputs } from '$lib/trpc/router';
+	import { getModalState } from '$lib/components/modal';
 
 	export let data: PageData;
 	$: ({ user, collections } = data);
 
+	$: favCollections = getFavCollections(collections);
 	$: updCollections = getUpdCollections(collections);
 
+	const crtCollectionModal = getModalState();
+
 	type CollectionArray = RouterOutputs['collections']['list'];
+	function getFavCollections(collections: CollectionArray) {
+		return collections.filter((collection) => collection.isFavourite);
+	}
+
 	function getUpdCollections(collections: CollectionArray) {
 		const sorted = collections.sort((a, b) => b.updatedAt.getTime() - a.updatedAt.getTime());
 
@@ -29,30 +37,50 @@
 	<PageContent class="space-y-5">
 		<h1 class="font-semibold text-2xl">Welcome back, {user.name}!</h1>
 
-		<section class="space-y-1.5">
-			<h2 class="text-xl">Favourites Collections</h2>
+		{#if favCollections.length > 0}
+			<section class="space-y-1.5">
+				<h2 class="text-xl">Favourites Collections</h2>
 
-			<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
-				{#each collections as collection (collection.id)}
-					{#if collection.isFavourite}
+				<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
+					{#each favCollections as collection (collection.id)}
 						<CollectionOverview {collection} />
-					{/if}
-				{/each}
-			</div>
-		</section>
+					{/each}
+				</div>
+			</section>
+		{/if}
 
-		<section class="mt-10 space-y-1.5">
-			<div class="flex items-centers justify-between">
-				<h2 class="text-xl">Recently updated collections</h2>
+		{#if updCollections.length > 0}
+			<section class="mt-10 space-y-1.5">
+				<div class="flex items-centers justify-between">
+					<h2 class="text-xl">Recently updated collections</h2>
 
-				<Button href="/collections" variant="ghost" size="icon"><ArrowRight /></Button>
-			</div>
+					<Button href="/collections" variant="ghost" size="icon"><ArrowRight /></Button>
+				</div>
 
-			<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
-				{#each updCollections as collection}
-					<CollectionOverview {collection} />
-				{/each}
+				<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
+					{#each updCollections as collection}
+						<CollectionOverview {collection} />
+					{/each}
+				</div>
+			</section>
+		{:else}
+			<div class="grid grid-cols-1 gap-2 max-w-lg mx-auto">
+				<div class="flex flex-col items-center justify-between py-10">
+					<Egg class="size-16 fill-primary text-primary" />
+
+					<p class="text-xl font-medium">Wow, such empty</p>
+				</div>
+
+				<Button on:click={() => ($crtCollectionModal = true)} class="h-12 w-full">
+					<FolderPlus />
+					<span> Create Collection</span>
+				</Button>
+
+				<Button href="/templates" variant="secondary" class="h-12 w-full">
+					<Dna />
+					<span> Browser Templates</span>
+				</Button>
 			</div>
-		</section>
+		{/if}
 	</PageContent>
 </PageContainer>
