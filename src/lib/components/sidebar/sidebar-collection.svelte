6ed8ec033e435fce5cb629cf73c9c901ec7b3ee7
M1NMAX <josecferreira.alt@gmail.com>
2024-01-17 19:45:05 +0000

    test drawer in sidebar

--- src/lib/components/sidebar/sidebar-collection.svelte
+++ src/lib/components/sidebar/sidebar-collection.svelte
@@ -20,6 +20,7 @@
 	import { mediaQuery } from 'svelte-legos';
 	import { getSidebarState } from './index.js';
 	import { goto } from '$app/navigation';
+	import * as Drawer from '$lib/components/ui/drawer';
 
 	export let active: boolean;
 	export let asChild: boolean = false;
@@ -27,6 +28,7 @@
 	export let groups: { id: string; name: string }[];
 	$: ({ id, name, icon } = collection);
 
+	let isDrawerOpen = false;
 	let isRenamePopoverOpen = false;
 	let isGroupComboboxOpen = false;
 
@@ -123,55 +125,103 @@
 		</Popover.Content>
 	</Popover.Root>
 
-	<DropdownMenu.Root>
-		<DropdownMenu.Trigger asChild let:builder>
-			<Button
-				builders={[builder]}
-				variant="ghost"
-				size="xs"
-				class="invisible group-hover:visible transition-opacity"
-			>
-				<MoreHorizontal class="icon-xs" />
-			</Button>
-		</DropdownMenu.Trigger>
-		<DropdownMenu.Content class="w-56">
-			<DropdownMenu.Item class="space-x-2" on:click={() => (isRenamePopoverOpen = true)}>
-				<Pencil class="icon-xs" />
-				<span> Rename </span>
-			</DropdownMenu.Item>
+	{#if $isDesktop}
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger asChild let:builder>
+				<Button
+					builders={[builder]}
+					variant="ghost"
+					size="xs"
+					class="invisible group-hover:visible transition-opacity"
+				>
+					<MoreHorizontal class="icon-xs" />
+				</Button>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content class="w-56">
+				<DropdownMenu.Item class="space-x-2" on:click={() => (isRenamePopoverOpen = true)}>
+					<Pencil class="icon-xs" />
+					<span> Rename </span>
+				</DropdownMenu.Item>
 
-			<DropdownMenu.Item
-				class="space-x-2"
-				on:click={() => {
-					dispatch('toggleFavourite', { id, value: !collection.isFavourite });
-				}}
-			>
-				{#if collection.isFavourite}
-					<HeartOff class="icon-xs" />
-					<span> Remove from Favourites </span>
-				{:else}
-					<Heart class="icon-xs" />
-					<span> Add to Favourites </span>
-				{/if}
-			</DropdownMenu.Item>
+				<DropdownMenu.Item
+					class="space-x-2"
+					on:click={() => {
+						dispatch('toggleFavourite', { id, value: !collection.isFavourite });
+					}}
+				>
+					{#if collection.isFavourite}
+						<HeartOff class="icon-xs" />
+						<span> Remove from Favourites </span>
+					{:else}
+						<Heart class="icon-xs" />
+						<span> Add to Favourites </span>
+					{/if}
+				</DropdownMenu.Item>
 
-			<DropdownMenu.Item class="space-x-2" on:click={() => (isGroupComboboxOpen = true)}>
-				<CornerUpRight class="icon-xs" />
-				<span>Move to</span>
-			</DropdownMenu.Item>
+				<DropdownMenu.Item class="space-x-2" on:click={() => (isGroupComboboxOpen = true)}>
+					<CornerUpRight class="icon-xs" />
+					<span>Move to</span>
+				</DropdownMenu.Item>
 
-			<DropdownMenu.Item class="space-x-2" on:click={() => dispatch('duplicateCollection', { id })}>
-				<Copy class="icon-xs" />
-				<span>Duplicate</span>
-			</DropdownMenu.Item>
+				<DropdownMenu.Item
+					class="space-x-2"
+					on:click={() => dispatch('duplicateCollection', { id })}
+				>
+					<Copy class="icon-xs" />
+					<span>Duplicate</span>
+				</DropdownMenu.Item>
 
-			<DropdownMenu.Item
-				class="space-x-2"
-				on:click={() => dispatch('deleteCollection', { id, name })}
-			>
-				<Trash class="icon-xs" />
-				<span>Delete</span>
-			</DropdownMenu.Item>
-		</DropdownMenu.Content>
-	</DropdownMenu.Root>
+				<DropdownMenu.Item
+					class="space-x-2"
+					on:click={() => dispatch('deleteCollection', { id, name })}
+				>
+					<Trash class="icon-xs" />
+					<span>Delete</span>
+				</DropdownMenu.Item>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+	{/if}
+
+	{#if !$isDesktop}
+		<Button size="icon" variant="ghost" on:click={() => (isDrawerOpen = true)}>
+			<MoreHorizontal />
+		</Button>
+	{/if}
 </span>
+
+{#if !$isDesktop}
+	<Drawer.Root bind:open={isDrawerOpen}>
+		<Drawer.Content>
+			<Drawer.Header>
+				<form class="w-full">
+					<label for="name" class=" sr-only"> Name </label>
+					<input id="name" value={name} name="name" class="input" on:keydown={handleKeydown} />
+				</form>
+			</Drawer.Header>
+			<Drawer.Footer>
+				<Button
+					variant="secondary"
+					on:click={() => {
+						dispatch('toggleFavourite', { id, value: !collection.isFavourite });
+					}}
+				>
+					{#if collection.isFavourite}
+						<HeartOff class="icon-xs" />
+						<span> Remove from Favourites </span>
+					{:else}
+						<Heart class="icon-xs" />
+						<span> Add to Favourites </span>
+					{/if}
+				</Button>
+				<Button variant="secondary" on:click={() => dispatch('duplicateCollection', { id })}>
+					<Copy class="icon-xs" />
+					<span>Duplicate</span>
+				</Button>
+				<Button variant="secondary" on:click={() => dispatch('deleteCollection', { id, name })}>
+					<Trash class="icon-xs" />
+					<span>Delete</span>
+				</Button>
+			</Drawer.Footer>
+		</Drawer.Content>
+	</Drawer.Root>
+{/if}
