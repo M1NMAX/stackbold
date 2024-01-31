9a2f4ae7d0c52287d05c4267576049bfc0255019
M1NMAX <josecferreira.alt@gmail.com>
2024-01-31 09:53:43 +0000

    test: try new timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
 	webServer: {
 		command: 'npm run dev',
 		url: 'http://127.0.0.1:5173',
-		reuseExistingServer: !process.env.CI
+		timeout: 120000
 	},
 
 	/* Configure projects for major browsers */
