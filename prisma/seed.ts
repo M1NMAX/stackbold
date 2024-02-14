43f2af3f8523de43523633b9249cff1b717e2d9d
M1NMAX <josecferreira.alt@gmail.com>
2024-02-14 11:47:39 +0000

    chore: use more realistic data form seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,11 +7,118 @@ const colorsNames = ['RED', 'BLUE', 'GREEN'];
 const templatesData = [
 	{
 		name: 'Events',
-		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+		icon: 'calendar',
+		description:
+			'Organize and manage events effortlessly with details like dates, locations, guest lists, and agendas.',
 		properties: [
 			{
 				name: 'Date',
 				type: 'DATE' as PropertyType,
+				options: []
+			},
+			{
+				name: 'Location',
+				type: 'TEXT' as PropertyType,
+				options: []
+			},
+			{
+				name: 'Note',
+				type: 'TEXT' as PropertyType,
+				options: []
+			}
+		],
+		items: [
+			{
+				name: 'Bob birthdate',
+				properties: [
+					{ value: '2024-06-24' },
+					{ value: '1745 T Street Southeast, Washington' },
+					{ value: 'The Surprise party starts at 06:00 PM. Remember the gift.' }
+				]
+			},
+			{
+				name: 'Alice birthdate',
+				properties: [
+					{ value: '2024-04-25' },
+					{ value: '6007 Applegate Lane, Louisville' },
+					{ value: 'I am responsible for buying the cake. Chocolate cake.' }
+				]
+			},
+			{
+				name: 'Susam wedding',
+				properties: [
+					{ value: '2024-07-25' },
+					{ value: '560 Penstock Drive, Grass Valley' },
+					{ value: 'Finish the decision on what I will wear. Try to match with my plus one.' }
+				]
+			}
+		]
+	},
+	{
+		name: 'Tasks',
+		icon: 'taskBook',
+
+		description:
+			'Efficiently manage your tasks by utilizing features such as prioritization, setting deadlines, and tracking progress.',
+		properties: [
+			{
+				name: 'Priority',
+				type: 'SELECT' as PropertyType,
+				options: [{ value: 'Low' }, { value: 'Medium' }, { value: 'High' }]
+			},
+			{
+				name: 'Deadline',
+				type: 'DATE' as PropertyType,
+				options: []
+			},
+			{
+				name: 'Status',
+				type: 'SELECT' as PropertyType,
+				options: [
+					{ value: 'Todo' },
+					{ value: 'In Progress' },
+					{ value: 'Done' },
+					{ value: 'Canceled' }
+				]
+			}
+		],
+		items: [
+			{
+				name: 'We need to hack the redundant UTF8 transmitter!',
+				properties: [{ value: 'Low' }, { value: '2024-04-24' }, { value: 'Todo' }]
+			},
+			{
+				name: 'We need to generate the virtual HEX alarm!',
+				properties: [{ value: 'Low' }, { value: '2024-08-25' }, { value: 'In Progress' }]
+			},
+
+			{
+				name: 'I`ll parse the wireless SSL protocol, that should driver the API panel!',
+				properties: [{ value: 'Medium' }, { value: '2024-08-25' }, { value: 'Canceled' }]
+			},
+			{
+				name: 'You can`t generate the capacitor without indexing the wireless HEX pixel!',
+				properties: [{ value: 'Low' }, { value: '2024-08-25' }, { value: 'Canceled' }]
+			},
+			{
+				name: 'Use the digital TLS panel, then you can transmit the haptic system!',
+				properties: [{ value: 'High' }, { value: '2024-08-25' }, { value: 'Done' }]
+			},
+			{
+				name: 'We need to bypass the neural TCP card!',
+				properties: [{ value: 'High' }, { value: '2024-08-25' }, { value: 'Todo' }]
+			}
+		]
+	},
+	{
+		name: 'ToDo',
+		icon: 'todo',
+		description:
+			'Manage your todos with this straightforward template. Track what is complete and what remains, with an additional node for each.',
+		properties: [
+			{
+				name: 'Done',
+				type: 'CHECKBOX' as PropertyType,
 				options: []
 			},
 			{
@@ -22,81 +129,46 @@ const templatesData = [
 		],
 		items: [
 			{
-				name: 'Apple WWDC',
-				properties: [{ value: '2022-06-24' }, { value: '06:00 PM' }]
-			},
-			{
-				name: 'S birthdate',
-				properties: [{ value: '2022-08-25' }, { value: 'I need to buy a chocolate cake' }]
-			}
-		]
-	},
-	{
-		name: 'Tasks',
-
-		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-		properties: [
-			{
-				name: 'Done',
-				type: 'CHECKBOX' as PropertyType,
-				options: []
-			},
-			{
-				name: 'Conclusion date',
-				type: 'DATE' as PropertyType,
-				options: []
-			},
-			{
-				name: 'Notes',
-				type: 'DATE' as PropertyType,
-				options: []
-			}
-		],
-		items: [
-			{
 				name: 'Organize the agenda',
-				properties: [{ value: 'true' }, { value: '2022-06-24' }, { value: 'Start from september' }]
+				properties: [{ value: 'true' }, { value: 'Start from april' }]
 			},
 			{
 				name: 'Change battery of smoke detecter',
-				properties: [{ value: '' }, { value: '2022-08-25' }, { value: 'I John for help' }]
-			}
-		]
-	},
-	{
-		name: 'Links',
-		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-		properties: [
-			{
-				name: 'url',
-				type: 'URL' as PropertyType,
-				options: []
-			}
-		],
-		items: [
-			{
-				name: 'Shadcn-svelte-ui',
-				properties: [{ value: 'https://www.shadcn-svelte.com/docs' }]
+				properties: [{ value: 'false' }, { value: 'I John for help' }]
 			},
 			{
-				name: 'Headless ui',
-				properties: [{ value: 'https://headlessui.dev' }]
+				name: 'Invite some friends over for a game night',
+				properties: [{ value: 'false' }, { value: '' }]
+			},
+			{
+				name: 'Write a thank you letter to an influential person in my life',
+				properties: [{ value: 'true' }, { value: '' }]
 			}
 		]
 	},
-
 	{
 		name: 'Movies',
-		icon: { name: 'FilmIcon' },
-		description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+		icon: 'film',
+		description:
+			'Easily track movies to watch, categorize them by genre, and mark them as watched with additional detail like ratings.',
 		properties: [
+			{
+				name: 'Genre',
+				type: 'SELECT' as PropertyType,
+				options: [
+					{ value: 'Action' },
+					{ value: 'Comedy' },
+					{ value: 'Drama' },
+					{ value: 'Romance' }
+				]
+			},
 			{
 				name: 'Status',
 				type: 'SELECT' as PropertyType,
-				options: [{ value: 'Plan to watch' }, { value: 'Watching' }, { value: 'Watched' }]
+				options: [{ value: 'Plan to watch' }, { value: 'Watched' }]
 			},
 			{
-				name: 'Score',
+				name: 'Rating',
 				type: 'SELECT' as PropertyType,
 				options: [
 					{ value: '1-Appaling' },
@@ -110,27 +182,36 @@ const templatesData = [
 					{ value: '9-Great' },
 					{ value: '10-Masterpiece' }
 				]
-			},
-			{
-				name: 'Plataform',
-				type: 'SELECT' as PropertyType,
-				options: [{ value: 'Netflix' }, { value: 'Vizer.tv' }, { value: 'Stremio' }]
 			}
 		],
 		items: [
 			{
-				name: 'Taken',
-				properties: [
-					{
-						value: 'Plan to watch'
-					},
-					{ value: '' },
-					{ value: 'Vizer.tv' }
-				]
+				name: 'Taken (2008)',
+				properties: [{ value: 'Action' }, { value: 'Plan to watch' }, { value: '' }]
 			},
 			{
 				name: 'The accountant',
-				properties: [{ value: 'Watched' }, { value: '7-Good' }, { value: 'Stremio' }]
+				properties: [{ value: 'Action' }, { value: 'Watched' }, { value: '7-Good' }]
+			},
+			{
+				name: 'Titanic',
+				properties: [{ value: 'Drama' }, { value: 'Watched' }, { value: '8-Very Good' }]
+			},
+			{
+				name: 'The Princess Bride',
+				properties: [{ value: 'Comedy' }, { value: 'Watched' }, { value: '8-Very Good' }]
+			},
+			{
+				name: 'The Ugly Truth',
+				properties: [{ value: 'Romance' }, { value: 'Watched' }, { value: '6-Fine' }]
+			},
+			{
+				name: 'Napoleon',
+				properties: [{ value: 'Action' }, { value: 'Plan to watch' }, { value: '6-Fine' }]
+			},
+			{
+				name: 'The Truman Show',
+				properties: [{ value: 'Comedy' }, { value: 'Watched' }, { value: '8-Very Good' }]
 			}
 		]
 	}
@@ -147,9 +228,10 @@ async function main() {
 	await prisma.template.deleteMany();
 
 	for (const template of templatesData) {
-		const { name, description, properties, items } = template;
+		const { icon, name, description, properties, items } = template;
 		const createdTemplate = await prisma.template.create({
 			data: {
+				icon,
 				name,
 				description,
 				properties: properties.map(({ name, type, options }) => ({
