8910dadf80dd13f1cb3db2f65d5bac3a99869d9d
M1NMAX <josecferreira.alt@gmail.com>
2024-08-16 09:16:43 +0100

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-
 import { createRequire } from 'module';
 import path from 'path';
 
@@ -16,5 +15,10 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	resolve: {
 		alias: { '.prisma/client/index-browser': path.relative(__dirname, prismaClientIndexBrowser) }
-	}
+	},
+	worker: {
+		rollupOptions: {
+			external: ['@node-rs/argon2']
+		}
+	},
 });
