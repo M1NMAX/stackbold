2d28aee724e5a25c970e3d9a6b3ab69d4841ae76
M1NMAX <josecferreira.alt@gmail.com>
2024-10-20 10:45:16 +0100

    ref and upd sliding panel to use svelte 5 api

--- src/lib/components/sliding-panel/sliding-panel.svelte
+++ src/lib/components/sliding-panel/sliding-panel.svelte
@@ -2,26 +2,25 @@
 	import { fly } from 'svelte/transition';
 	import { sineIn } from 'svelte/easing';
 	import { cn } from '$lib/utils';
+	import type { Snippet } from 'svelte';
 
-	export let id: string = 'drawer-id';
-	export let open: boolean = false;
+	type Props = {
+		open: boolean;
+		class?: string;
+		children: Snippet;
+	};
 
-	let className: string | undefined = undefined;
-	export { className as class };
+	let { open, class: className, children }: Props = $props();
 </script>
 
 {#if open}
 	<div
-		{id}
-		{...$$restProps}
-		class={cn('absolute inset-y-0 right-0 z-20 w-80 p-1 overflow-y-auto bg-secondary', className)}
+		class={cn(
+			'w-80 h-full flex flex-col space-y-1.5 p-1.5 overflow-hidden rounded-md bg-card',
+			className
+		)}
 		transition:fly={{ x: 320, duration: 300, easing: sineIn }}
-		tabindex="-1"
-		aria-controls={id}
-		aria-labelledby={id}
 	>
-		<div class="h-full flex flex-col space-y-1.5 p-2 rounded-md bg-card">
-			<slot {open} />
-		</div>
+		{@render children()}
 	</div>
 {/if}
