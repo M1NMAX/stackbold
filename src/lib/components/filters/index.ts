0abf2b1e78355b686365506ad117300835c10acc
M1NMAX <josecferreira.alt@gmail.com>
2024-10-27 10:12:52 +0000

    move view btns to filters

--- src/lib/components/filters/index.ts
+++ src/lib/components/filters/index.ts
@@ -3,6 +3,7 @@ export { default as GroupByMenu } from './group-by-menu.svelte';
 export { default as SearchInput } from './search-input.svelte';
 export { default as SortMenu } from './sort-menu.svelte';
 export { default as SortArrow } from './sort-arrow.svelte';
+export { default as ViewButtons } from './view-buttons.svelte';
 
 export * from './helpers';
-export * from './context';
+export * from './sortState';
--- src/lib/components/view/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ViewButtonsGroup } from './view-button-group.svelte';
-
-export * from './context';
