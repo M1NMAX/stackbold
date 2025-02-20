2b44f24d7cac2e5e17dedd12c6c76cbee9ee4984
M1NMAX <josecferreira.alt@gmail.com>
2025-02-20 19:48:38 +0000

    new component: calendar

---
+++ src/lib/components/base/calendar.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	import ChevronLeft from 'lucide-svelte/icons/chevron-left';
+	import ChevronRight from 'lucide-svelte/icons/chevron-right';
+	import { CalendarDate, getLocalTimeZone, today } from '@internationalized/date';
+	import { buttonVariants } from './index.js';
+	import { createMonth } from '$lib/states/index.js';
+	import { WEEK_DAYS } from '$lib/constant/index.js';
+
+	type Props = {
+		value?: CalendarDate;
+		onchange?: (date: CalendarDate) => void;
+	};
+
+	let { value = $bindable(today(getLocalTimeZone())), onchange }: Props = $props();
+
+	let pointer = $state(value);
+	const month = $derived(createMonth(pointer, value));
+
+	function previousMonth() {
+		pointer = pointer.subtract({ months: 1 });
+	}
+
+	function nextMonth() {
+		pointer = pointer.add({ months: 1 });
+	}
+
+	function onClickDay(day: CalendarDate) {
+		value = day;
+		onchange?.(value);
+	}
+</script>
+
+<div class="max-w-xs flex flex-col gap-y-2 p-2 rounded-md bg-background">
+	<div class="flex items-center justify-between">
+		<button class={buttonVariants({ theme: 'outline', variant: 'icon' })} onclick={previousMonth}>
+			<ChevronLeft />
+		</button>
+
+		<span class="text-sm font-medium">
+			{month.title}
+		</span>
+
+		<button class={buttonVariants({ theme: 'outline', variant: 'icon' })} onclick={nextMonth}>
+			<ChevronRight />
+		</button>
+	</div>
+
+	<div class="flex items-center justify-between">
+		{#each Object.keys(WEEK_DAYS) as key (key)}
+			<span
+				class={[
+					'size-9 flex items-center justify-center',
+					'text-[0.8rem] font-normal text-muted-foreground'
+				]}
+			>
+				{WEEK_DAYS[+key].slice(0, 2)}
+			</span>
+		{/each}
+	</div>
+	<div class="grid grid-cols-7 grid-flow-rows">
+		{#each month.days as day}
+			<button
+				disabled={day.isDisabled}
+				onclick={() => onClickDay(day.date)}
+				class={[
+					buttonVariants({
+						theme: 'ghost',
+						variant: 'icon'
+					}),
+					'size-9 flex items-center justify-center rounded-md text-sm',
+					day.isSelected && 'bg-primary hover:bg-primary/90',
+					day.isSelected && day.isDisabled && 'bg-accent/70',
+					day.isToday && 'bg-accent'
+				]}
+			>
+				{day.date.day}
+			</button>
+		{/each}
+	</div>
+</div>
---
+++ src/lib/states/date-time-state.svelte.ts
@@ -0,0 +1,61 @@
+import { LOCALE } from '$lib/constant';
+import {
+	DateFormatter,
+	endOfMonth,
+	endOfWeek,
+	getLocalTimeZone,
+	isSameDay,
+	isSameMonth,
+	isToday,
+	startOfMonth,
+	startOfWeek,
+	type CalendarDate
+} from '@internationalized/date';
+
+class Day {
+	date: CalendarDate;
+	isSelected: boolean;
+	isToday: boolean;
+	isDisabled: boolean;
+
+	constructor(date: CalendarDate, isSelected: boolean, isToday: boolean, isDisabled: boolean) {
+		this.date = date;
+		this.isSelected = isSelected;
+		this.isToday = isToday;
+		this.isDisabled = isDisabled;
+	}
+}
+
+class Month {
+	title: string;
+	days: Day[];
+	constructor(days: Day[], title: string) {
+		this.days = days;
+		this.title = title;
+	}
+}
+
+export function fullMonthAndYear(date: Date) {
+	return new DateFormatter(LOCALE, { month: 'long', year: 'numeric' }).format(date);
+}
+
+export function createMonth(ref: CalendarDate, value: CalendarDate) {
+	let days: Day[] = [];
+
+	const end = endOfWeek(endOfMonth(ref), LOCALE);
+	let current = startOfWeek(startOfMonth(ref), LOCALE);
+
+	while (current.compare(end) <= 0) {
+		days.push(
+			new Day(
+				current,
+				isSameDay(current, value),
+				isToday(current, getLocalTimeZone()),
+				!isSameMonth(current, ref)
+			)
+		);
+		current = current.add({ days: 1 });
+	}
+
+	return new Month(days, fullMonthAndYear(ref.toDate(getLocalTimeZone())));
+}
