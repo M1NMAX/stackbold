35ee89ac8167fe4d8b9d53b2bc59ad4d76a39e83
M1NMAX <josecferreira.alt@gmail.com>
2025-03-03 22:44:32 +0000

    remove svelte-sequential-preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
-import sequence from 'svelte-sequential-preprocessor';
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
-	preprocess: sequence([vitePreprocess()]),
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
 		alias: {
