2980553bf6b1437cc2330739eb9da0a818b04b18
M1NMAX <josecferreira.alt@gmail.com>
2025-05-10 18:58:31 +0100

    minor change

--- src/lib/components/page/page-content.svelte
+++ src/lib/components/page/page-content.svelte
@@ -14,7 +14,7 @@
 <div
 	{onscroll}
 	class={tm(
-		'h-full w-full flex flex-col mx-auto pt-0.5 pb-2 px-2 md:px-4 gap-y-2 overflow-y-auto',
+		'h-full w-full flex flex-col mx-auto pt-0.5 pb-2 px-2 md:px-4 gap-y-2 overflow-x-hidden overflow-y-auto',
 		className
 	)}
 >
