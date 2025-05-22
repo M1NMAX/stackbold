60a99d57710c619857833de79c60c0043ed868eb
M1NMAX <josecferreira.alt@gmail.com>
2025-05-22 19:29:53 +0100

    ref property schema

--- prisma/migration.ts
+++ prisma/migration.ts
@@ -10,10 +10,40 @@ async function migrate() {
 			const property = collection.properties[i];
 			console.log('Property ', property.name, ' id ', property.id);
 
+			const { id, ...rest } = property;
+			const createdProperty = await prisma.propertyTmp.create({
+				data: {
+					collectionId: collection.id,
+					...rest
+				}
+			});
+
+			console.log('Created new property ', property.id, 'with new id ', createdProperty.id);
+
 			await prisma.collection.update({
 				where: { id: collection.id },
-				data: { properties: { updateMany: { where: { id: property.id }, data: { order: i + 1 } } } }
+				data: { properties: { deleteMany: { where: { id: property.id } } } }
 			});
+			console.log('Removed property ', property.id, ' from collection ', collection.id);
+
+			console.log('Updating item property refs');
+			const items = await prisma.item.findMany({ where: { collectionId: collection.id } });
+
+			for (const item of items) {
+				console.log('Updating item ', item.id);
+				const ref = item.properties.find((prop) => prop.id === property.id);
+				if (!ref) continue;
+				console.log('Replacing ref id ', ref.id);
+				await prisma.item.update({
+					where: { id: item.id },
+					data: { properties: { push: { id: createdProperty.id, value: ref.value } } }
+				});
+
+				await prisma.item.update({
+					where: { id: item.id },
+					data: { properties: { deleteMany: { where: { id: property.id } } } }
+				});
+			}
 		}
 	}
 }
