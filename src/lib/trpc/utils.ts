2187c4a0b47d042e88cbc1207fd0c7ba0f4f0779
M1NMAX <josecferreira.alt@gmail.com>
2025-07-10 07:24:46 +0100

    ref: replace string with prisma enum

--- src/lib/trpc/utils.ts
+++ src/lib/trpc/utils.ts
@@ -1,44 +1,49 @@
-import type { Aggregator, Item, Property, PropertyRef, PropertyType } from '@prisma/client';
-import { map } from '@trpc/server/observable';
+import {
+	Aggregator,
+	PropertyType,
+	type Item,
+	type Property,
+	type PropertyRef
+} from '@prisma/client';
 
 export function isRelation(property: Property) {
-	return property.type === 'RELATION' && property.targetCollection !== '';
+	return property.type === PropertyType.RELATION && property.targetCollection !== '';
 }
 
 export function isBundle(property: Property) {
 	return (
-		property.type === 'BUNDLE' &&
+		property.type === PropertyType.BUNDLE &&
 		property.intTargetProperty !== '' &&
 		property.targetCollection !== ''
 	);
 }
 
-const PROPERTY_TYPES_WITHOUT_REF = ['CREATED', 'BUNDLE'];
+const PROPERTY_TYPES_WITHOUT_REF: PropertyType[] = [PropertyType.CREATED, PropertyType.BUNDLE];
 export function hasRef(type: PropertyType) {
 	return !PROPERTY_TYPES_WITHOUT_REF.includes(type);
 }
 
 export function aggregatePropertyValue(items: Item[], aggregator: Aggregator, pid: string) {
-	if (aggregator === 'COUNT') return items.length;
-	else if (aggregator === 'COUNT_EMPTY') {
+	if (aggregator === Aggregator.COUNT) return items.length;
+	else if (aggregator === Aggregator.COUNT_EMPTY) {
 		return items.reduce((acc, item) => {
 			const propertyRef = getPropertyRef(item.properties, pid);
 			if (propertyRef == null || propertyRef.value === '') return acc + 1;
 			return acc;
 		}, 0);
-	} else if (aggregator === 'COUNT_NOT_EMPTY') {
+	} else if (aggregator === Aggregator.COUNT_NOT_EMPTY) {
 		return items.reduce((acc, item) => {
 			const propertyRef = getPropertyRef(item.properties, pid);
 			if (propertyRef && propertyRef.value !== '') return acc + 1;
 			return acc;
 		}, 0);
-	} else if (aggregator === 'SUM' || aggregator === 'AVG') {
+	} else if (aggregator === Aggregator.SUM || aggregator === Aggregator.AVG) {
 		const sum = items.reduce((acc, curr) => {
 			const propertyRef = getPropertyRef(curr.properties, pid);
 			const inc = propertyRef ? propertyRef.value : 0;
 			return acc + Number(inc);
 		}, 0);
-		if (aggregator === 'SUM') return sum.toFixed(2);
+		if (aggregator === Aggregator.SUM) return sum.toFixed(2);
 		return (sum / items.length).toFixed(2);
 	}
 	return '';
