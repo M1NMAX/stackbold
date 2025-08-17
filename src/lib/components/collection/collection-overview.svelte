a730799906176d8cb61eb219205ec43ba2543edc
M1NMAX <josecferreira.alt@gmail.com>
2025-08-17 16:36:26 +0100

    recovery: collection overview

--- src/lib/components/collection/collection-overview.svelte
+++ src/lib/components/collection/collection-overview.svelte
@@ -2,7 +2,7 @@
 	import type { Collection } from '@prisma/client';
 	import dayjs from '$lib/utils/dayjs';
 	import FileClock from 'lucide-svelte/icons/file-clock';
-	import { COLLECTION_ICONS } from '$lib/constant';
+	import { COLLECTION_ICONS, DEFAULT_VIEW_SHORT_ID } from '$lib/constant/index.js';
 
 	type Props = {
 		collection: Collection;
@@ -11,10 +11,17 @@
 	let { collection }: Props = $props();
 
 	const Icon = $derived(COLLECTION_ICONS[collection.icon]);
+
+	function getCollectionView() {
+		if (typeof window === 'undefined') return DEFAULT_VIEW_SHORT_ID.toString();
+		const key = `collection-${collection.id}-view`;
+		const stored = localStorage.getItem(key);
+		return stored ? JSON.parse(stored) : DEFAULT_VIEW_SHORT_ID.toString();
+	}
 </script>
 
 <a
-	href="/collections/{collection.id}"
+	href="/collections/{collection.id}?view={getCollectionView()}"
 	data-testid="collection-overview"
 	class="flex flex-col items-start p-1.5 space-y-2 rounded bg-secondary bg-opacity-80 dark:bg-opacity-40 hover:bg-secondary/40 dark:hover:bg-secondary/60"
 >
