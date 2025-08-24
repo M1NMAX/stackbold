97944728a2621603f48171a53d3b09366734efe7
M1NMAX <josecferreira.alt@gmail.com>
2025-08-24 15:06:00 +0100

    turn view selector responsive

--- src/lib/components/view/view-buttons.svelte
+++ src/lib/components/view/view-buttons.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
 	import type { View } from '@prisma/client';
-	import { tm } from '$lib/utils/index.js';
-	import { Label, RadioGroup, RadioGroupItem } from '$lib/components/base/index.js';
+	import { tm, useId } from '$lib/utils/index.js';
+	import {
+		buttonVariants,
+		Drawer,
+		Label,
+		MenuTitle,
+		RadioGroup,
+		RadioGroupItem
+	} from '$lib/components/base/index.js';
 	import { VIEW_ICONS } from '$lib/constant/index.js';
+	import { ModalState } from '$lib/states/index.js';
 
 	type Props = {
 		views: View[];
@@ -12,9 +20,18 @@
 	};
 
 	let { views, value, onchange, class: className }: Props = $props();
+	const view = $derived.by(() => views.find((v) => v.shortId.toString() === value)!);
+	const CurrentIcon = $derived(VIEW_ICONS[view.type.toLowerCase()]);
+
+	const id = useId('collection-views');
+	const menuState = new ModalState();
 </script>
 
-<RadioGroup {value} {onchange} class={tm('h-9 flex gap-0.5 rounded-md bg-secondary/50', className)}>
+<RadioGroup
+	{value}
+	{onchange}
+	class={tm('hidden md:flex h-9 gap-0.5 rounded-md bg-secondary/50', className)}
+>
 	{#each views as view}
 		{@const Icon = VIEW_ICONS[view.type.toLowerCase()]}
 
@@ -31,3 +48,35 @@
 		</Label>
 	{/each}
 </RadioGroup>
+
+<div class="block md:hidden">
+	<button
+		{id}
+		type="button"
+		onclick={() => menuState.toggle()}
+		class={buttonVariants({ theme: 'secondary' })}
+	>
+		<CurrentIcon />
+		<span>{view.name} </span>
+	</button>
+
+	<Drawer bind:open={menuState.isOpen}>
+		<MenuTitle title="Views" />
+		<RadioGroup
+			{value}
+			onchange={(v) => {
+				menuState.close();
+				onchange(v);
+			}}
+		>
+			{#each views as view}
+				{@const Icon = VIEW_ICONS[view.type.toLowerCase()]}
+				<Label for={view.id} compact hoverEffect>
+					<Icon />
+					<span class="grow">{view.name} </span>
+					<RadioGroupItem id={view.id} value={view.shortId.toString()}></RadioGroupItem>
+				</Label>
+			{/each}
+		</RadioGroup>
+	</Drawer>
+</div>
