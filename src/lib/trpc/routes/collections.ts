dc481959cad225fb6f4bd431b6e0a4a88a89c411
M1NMAX <josecferreira.alt@gmail.com>
2025-08-29 06:55:40 +0100

    fix duplicate collection

--- src/lib/trpc/routes/collections.ts
+++ src/lib/trpc/routes/collections.ts
@@ -2,7 +2,7 @@ import { createTRPCRouter, protectedProcedure } from '$lib/trpc/t';
 import { prisma } from '$lib/server/prisma';
 import { z } from 'zod';
 import { TRPCError } from '@trpc/server';
-import { PROPERTIES_WITHOUT_REF } from '$lib/constant/index.js';
+import { NAME_FIELD } from '$lib/constant/index.js';
 
 const collectionCreateSchema = z.object({
 	icon: z.string().optional(),
@@ -38,60 +38,9 @@ export const collections = createTRPCRouter({
 			});
 		}),
 
-	duplicate: protectedProcedure.input(z.string()).mutation(async ({ input: id }) => {
-		const target = await prisma.collection.findUniqueOrThrow({
-			where: { id },
-			include: { items: true, properties: true }
-		});
-
-		const { id: _1, createdAt: _2, updatedAt: _3, properties, items, ...rest } = target;
-
-		const collection = await prisma.collection.create({
-			data: {
-				...rest,
-				name: rest.name + ' copy',
-				filterConfigs: undefined,
-				groupByConfigs: undefined
-			},
-			include: { views: true }
-		});
-
-		const propertiesData = properties.map((property) => {
-			const { id: _1, createdAt: _2, updatedAt: _3, ...rest } = property;
-			return {
-				...rest,
-				collectionId: collection.id
-			};
-		});
-
-		await prisma.property.createMany({ data: propertiesData });
-
-		const createdProperies = await prisma.property.findMany({
-			where: {
-				collectionId: collection.id,
-				type: { notIn: PROPERTIES_WITHOUT_REF }
-			},
-			orderBy: { order: 'asc' }
-		});
-
-		const itemData = items.map((item) => {
-			const { id: _1, createdAt: _2, updatedAt: _3, ...rest } = item;
-
-			let refs = [];
-			for (let i = 0; i < createdProperies.length; i++) {
-				refs.push({
-					id: createdProperies[i].id,
-					value: rest.properties[i].value
-				});
-			}
-
-			return { ...rest, collectionId: collection.id, properties: refs };
-		});
-
-		await prisma.item.createMany({ data: itemData });
-
-		return collection;
-	}),
+	duplicate: protectedProcedure
+		.input(z.string())
+		.mutation(async ({ input }) => await duplicateCollection(input)),
 
 	update: protectedProcedure
 		.input(collectionUpdateSchema)
@@ -108,3 +57,110 @@ export const collections = createTRPCRouter({
 		await prisma.collection.delete({ where: { id } });
 	})
 });
+
+async function duplicateCollection(id: string) {
+	const target = await prisma.collection.findUniqueOrThrow({
+		where: { id },
+		include: {
+			views: true,
+			items: true,
+			properties: {
+				orderBy: { order: 'asc' }
+			}
+		}
+	});
+
+	const { id: _1, createdAt: _2, updatedAt: _3, views, properties, items, ...rest } = target;
+
+	const oldPropertyIdToOrder = new Map<number, string>(
+		properties.map((property) => [property.order, property.id])
+	);
+
+	const propertiesData = properties.map((property) => {
+		const { id: _1, createdAt: _2, updatedAt: _3, collectionId: _4, ...rest } = property;
+		return {
+			...rest
+		};
+	});
+
+	return await prisma.$transaction(async (tx) => {
+		const collection = await tx.collection.create({
+			data: {
+				...rest,
+				name: rest.name + ' copy',
+				filterConfigs: undefined,
+				groupByConfigs: undefined,
+				properties: { create: [...propertiesData] }
+			},
+			include: {
+				properties: { orderBy: { order: 'asc' } }
+			}
+		});
+
+		let propertyIdsMap = new Map<string, string>();
+		for (const property of collection.properties) {
+			const oldId = oldPropertyIdToOrder.get(property.order);
+			if (oldId) propertyIdsMap.set(oldId, property.id);
+		}
+
+		const viewsData = views.map((view) => {
+			const { id: _1, createdAt: _2, updatedAt: _3, collectionId: _4, ...rest } = view;
+
+			let groupBy = undefined;
+			if (rest.groupBy) {
+				const id = propertyIdsMap.get(rest.groupBy);
+				groupBy = id ?? undefined;
+			}
+
+			const sorts = rest.sorts.map((sort) => {
+				if (sort.field === NAME_FIELD) return { ...sort };
+				const id = propertyIdsMap.get(sort.field);
+				return id ? { ...sort, field: id } : null;
+			});
+
+			const filters = rest.filters.map((filter) => {
+				const id = propertyIdsMap.get(filter.id);
+				return id ? { ...filter, id } : null;
+			});
+
+			const properties = rest.properties.map((property) => {
+				const id = propertyIdsMap.get(property.id);
+				return id ? { ...property, id } : null;
+			});
+
+			return {
+				...rest,
+				groupBy,
+				collectionId: collection.id,
+				sorts: sorts.filter((s) => s != null),
+				filters: filters.filter((f) => f != null),
+				properties: properties.filter((p) => p != null)
+			};
+		});
+
+		const itemData = items.map((item) => {
+			const { id: _1, createdAt: _2, updatedAt: _3, ...rest } = item;
+
+			const refs = rest.properties.map((ref) => {
+				const id = propertyIdsMap.get(ref.id);
+				return id ? { ...ref, id } : null;
+			});
+
+			return {
+				...rest,
+				collectionId: collection.id,
+				properties: refs.filter((r) => r != null)
+			};
+		});
+
+		await Promise.all([
+			tx.view.createMany({ data: viewsData }),
+			tx.item.createMany({ data: itemData })
+		]);
+
+		return await tx.collection.findUniqueOrThrow({
+			where: { id: collection.id },
+			include: { views: true }
+		});
+	});
+}
