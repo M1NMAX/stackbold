7a9151e807ce4e48ad305ae7aeff42deeaa46e04
M1NMAX <josecferreira.alt@gmail.com>
2025-10-04 12:44:30 +0100

    add page title component

---
+++ src/lib/components/page/page-title.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { PAGE_ICONS } from '$lib/constant/index.js';
+	import { tm } from '$lib/utils/index.js';
+
+	type Props = {
+		icon: string;
+		title: string;
+		small?: boolean;
+		class?: string;
+	};
+	let { icon, title, small = false, class: className }: Props = $props();
+	const Icon = $derived(PAGE_ICONS[icon]);
+</script>
+
+<div class={tm('flex items-center gap-x-2', className)}>
+	<Icon class={tm(small ? 'size-4' : 'size-7')} />
+	<h1 class={tm('grow font-semibold', small ? 'text-lg' : 'text-3xl')}>{title}</h1>
+</div>
--- src/lib/constant/icons.ts
+++ src/lib/constant/icons.ts
@@ -55,12 +55,16 @@ import Gem from 'lucide-svelte/icons/gem';
 import Wallet from 'lucide-svelte/icons/wallet';
 
 //views
-
 import Kanban from 'lucide-svelte/icons/kanban';
 import StretchHorizontal from 'lucide-svelte/icons/stretch-horizontal';
 import Table from 'lucide-svelte/icons/table';
+
 // Others
 import File from 'lucide-svelte/icons/file';
+import Home from 'lucide-svelte/icons/home';
+import Library from 'lucide-svelte/icons/library';
+import Dna from 'lucide-svelte/icons/dna';
+import Bolt from 'lucide-svelte/icons/bolt';
 
 type IconBundle = {
 	[idx: string]: any;
@@ -133,7 +137,12 @@ export const VIEW_ICONS: IconBundle = {
 	board: Kanban
 };
 
-export const OTHERS_ICONS: IconBundle = {
+export const PAGE_ICONS: IconBundle = {
+	...COLLECTION_ICONS,
+	home: Home,
+	collections: Library,
+	templates: Dna,
+	settings: Bolt,
 	item: File
 };
 
@@ -143,5 +152,5 @@ export const APP_ICONS = {
 	...SORT_ICONS,
 	...COLLECTION_ICONS,
 	...VIEW_ICONS,
-	...OTHERS_ICONS
+	...PAGE_ICONS
 };
