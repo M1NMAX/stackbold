cc38f3adfe1a29bfb6adb5fc4f366e6396806a59
M1NMAX <josecferreira.alt@gmail.com>
2026-02-28 09:49:32 +0000

    ref item board view

--- src/lib/components/item/item-card.svelte
+++ src/lib/components/item/item-card.svelte
@@ -25,7 +25,7 @@
 	import debounce from 'debounce';
 	import { Button } from '$lib/components/base/index.js';
 	import { escapeKeydown, enterKeydown, clickOutside } from '$lib/actions/index.js';
-	import { getPropertyRef, isPropertyVisible, tm, useId } from '$lib/utils/index.js';
+	import { getPropertyRef, isPropertyVisible, tm } from '$lib/utils/index.js';
 	import type { ClickItemEvent } from '$lib/types.js';
 
 	type Props = {
@@ -35,9 +35,8 @@
 	};
 
 	let { view, item, clickOpenItem }: Props = $props();
-	let dragging = $state(false);
+	const draggable = $derived.by(() => view.type === ViewType.BOARD && view.groupBy != null);
 
-	const id = useId(`ìtem-${item.id}-card-`);
 	const propertyState = getPropertyState();
 	const itemState = getItemState();
 
@@ -73,14 +72,10 @@
 
 	function ondragstart(e: DragEvent) {
 		e.stopPropagation();
-		dragging = true;
 		if (!e.dataTransfer) return;
 		e.dataTransfer.effectAllowed = 'move';
 		e.dataTransfer.dropEffect = 'move';
 		e.dataTransfer.setData('text/plain', item.id);
-	}
-	function ondragend() {
-		dragging = false;
 	}
 
 	$effect(() => {
@@ -92,23 +87,21 @@
 
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <div
-	draggable={view.type === ViewType.BOARD}
+	{draggable}
 	tabindex="0"
 	role="button"
 	use:escapeKeydown
 	use:enterKeydown
-	onescapekey={(e) => saveAndClose(e.target as HTMLInputElement)}
-	onenterkey={(e) => saveAndClose(e.target as HTMLInputElement)}
+	onescapekey={(e) => saveAndClose(e.currentTarget as HTMLInputElement)}
+	onenterkey={(e) => saveAndClose(e.currentTarget as HTMLInputElement)}
 	{onclick}
 	{ondragstart}
-	{ondragend}
 	class={tm(
-		'relative w-full flex flex-col items-start p-2 gap-y-2 rounded-md bg-secondary bg-opacity-80 dark:bg-opacity-40 hover:bg-secondary/40 dark:hover:bg-secondary/60 group',
-		item.id === itemState.active && 'rounded-r-none border-r-2 border-primary bg-secondary/80',
-		dragging && 'text-blue-500'
+		'relative w-full flex flex-col items-start p-2 gap-y-2 rounded-md select-none bg-secondary bg-opacity-80 dark:bg-opacity-40 hover:bg-secondary/40 dark:hover:bg-secondary/60 group',
+		item.id === itemState.active && 'rounded-r-none border-r-2 border-primary bg-secondary/80'
 	)}
 >
-	{#if isCurrentlyEditing(id)}
+	{#if isCurrentlyEditing(item.id)}
 		<input
 			use:clickOutside
 			id={item.id}
@@ -138,7 +131,7 @@
 		<div
 			class="absolute right-1 top-1 flex items-center gap-x-1 lg:invisible lg:group-hover:visible"
 		>
-			<Button theme="ghost" onclick={() => startEditing(id)}>
+			<Button theme="ghost" onclick={() => startEditing(item.id)}>
 				<PencilLine />
 			</Button>
 			<ItemMenu id={item.id} name={item.name} {clickOpenItem} align="end" />
