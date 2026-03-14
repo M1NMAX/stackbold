8adc563449b5c62f5c12d52dd7d08625f0e4578b
M1NMAX <josecferreira.alt@gmail.com>
2026-03-14 17:23:23 +0000

    fix reset-password 2fa

--- src/routes/(auth)/reset-password/2fa/+page.server.ts
+++ src/routes/(auth)/reset-password/2fa/+page.server.ts
@@ -5,7 +5,7 @@ import {
 import { fail, redirect } from '@sveltejs/kit';
 import { superValidate } from 'sveltekit-superforms/server';
 import { zod4 as zod } from 'sveltekit-superforms/adapters';
-import { recoveryCodeSchema } from '$lib/schema';
+import { totpCodeSchema } from '$lib/schema';
 import type { Actions, PageServerLoad } from './$types';
 import { getUserTOTPKey } from '$lib/server/user';
 import { verifyTOTP } from '@oslojs/otp';
@@ -17,7 +17,7 @@ export const load: PageServerLoad = async (event) => {
 	if (!session.emailVerified) redirect(302, '/reset-password/verify-email');
 	if (!user.registered2FA || session.twoFactorVerified) redirect(302, '/reset-password');
 
-	const form = await superValidate(zod(recoveryCodeSchema));
+	const form = await superValidate(zod(totpCodeSchema));
 	return { form };
 };
 
@@ -29,7 +29,7 @@ export const actions: Actions = {
 		if (!session.emailVerified || !user.registered2FA || session.twoFactorVerified)
 			return fail(403, { message: 'Forbidden' });
 
-		const form = await superValidate(event.request, zod(recoveryCodeSchema));
+		const form = await superValidate(event.request, zod(totpCodeSchema));
 		if (!form.valid) return fail(400, { form });
 
 		const { code } = form.data;
