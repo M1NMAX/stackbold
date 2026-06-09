36f8b19ce4c544eb914f6709c9e04c6a8ceb50ba
M1NMAX <josecferreira.alt@gmail.com>
2026-06-09 14:46:33 +0100

    delete sidebar-open-btn.svelte

--- src/lib/components/sidebar/index.ts
+++ src/lib/components/sidebar/index.ts
@@ -1,7 +1,6 @@
 export { default as SidebarCollection } from './sidebar-collection.svelte';
 export { default as SidebarGroup } from './sidebar-group.svelte';
 export { default as SidebarItem } from './sidebar-item.svelte';
-export { default as SidebarOpenBtn } from './sidebar-open-btn.svelte';
 export { default as SidebarSearch } from './sidebar-search.svelte';
 
 export * from './sidebarState.svelte';
