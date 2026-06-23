9c10bc8448448d7ed687f053fc809c792ba95ef8
M1NMAX <josecferreira.alt@gmail.com>
2026-06-23 19:17:07 +0100

    upd badge component props

--- src/lib/components/base/badge.svelte
+++ src/lib/components/base/badge.svelte
@@ -5,13 +5,14 @@
 	import { THEME_COLORS } from '$lib/constant';
 
 	type Props = {
-		children: Snippet;
-		color?: Color;
-		class?: string;
 		id?: string;
+		color?: Color;
+		text?: string;
+		children?: Snippet;
+		class?: string;
 	};
 
-	let { id, children, color = Color.GRAY, class: className }: Props = $props();
+	let { id, text, children, color = Color.GRAY, class: className }: Props = $props();
 </script>
 
 <span
@@ -19,8 +20,15 @@
 	class={tm(
 		'w-fit h-6 lg:h-5 flex items-center gap-x-1 px-1 rounded-sm font-semibold text-sm [&_svg]:size-4 lg:[&_svg]:size-3.5',
 		THEME_COLORS[color],
+		text ? 'max-w-full text-ellipsis' : '',
 		className
 	)}
 >
-	{@render children()}
+	{#if text}
+		<span class="truncate min-w-0">
+			{text}
+		</span>
+	{:else if children}
+		{@render children()}
+	{/if}
 </span>
